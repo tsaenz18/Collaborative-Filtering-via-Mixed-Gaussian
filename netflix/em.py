d7660c401993e6188482faa5eb7f15a0a30f959c
tsaenz18 <59756025+tsaenz18@users.noreply.github.com>
2020-01-23 16:56:39 -0800

    Update em.py

--- netflix/em.py
+++ netflix/em.py
@@ -18,6 +18,20 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
         float: log-likelihood of the assignment
 
     """
+    n, _ = X.shape
+    K, _ = mixture.mu.shape
+    post = np.zeros((n, K))
+
+    ll = 0
+    for i in range(n):
+        for j in range(K):
+            likelihood = gaussian(X[i], mixture.mu[j], mixture.var[j])
+            post[i, j] = mixture.p[j] * likelihood
+        total = post[i, :].sum()
+        post[i, :] = post[i, :] / total
+        ll += np.log(total)
+
+    return post, ll
     raise NotImplementedError
 
 
@@ -37,6 +51,23 @@ def mstep(X: np.ndarray, post: np.ndarray, mixture: GaussianMixture,
     Returns:
         GaussianMixture: the new gaussian mixture
     """
+    n, d = X.shape
+    _, K = post.shape
+
+    n_hat = post.sum(axis=0)
+    p = n_hat / n
+
+    mu = np.zeros((K, d))
+    var = np.zeros(K)
+
+    for j in range(K):
+        # Computing mean
+        mu[j, :] = (X * post[:, j, None]).sum(axis=0) / n_hat[j]
+        # Computing variance
+        sse = ((mu[j] - X)**2).sum(axis=1) @ post[:, j]
+        var[j] = sse / (d * n_hat[j])
+
+    return GaussianMixture(mu, var, p)
     raise NotImplementedError
 
 
@@ -55,6 +86,14 @@ def run(X: np.ndarray, mixture: GaussianMixture,
             for all components for all examples
         float: log-likelihood of the current assignment
     """
+    prev_ll = None
+    ll = None
+    while (prev_ll is None or ll - prev_ll > 1e-6 * np.abs(ll)):
+        prev_ll = ll
+        post, ll = estep(X, mixture)
+        mixture = mstep(X, post, mixture)
+
+    return mixture, post, ll
     raise NotImplementedError
 
 
